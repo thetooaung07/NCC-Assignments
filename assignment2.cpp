57db872c0268f4c47c9a65b97bdf0ff3d017aad5
Thet Oo Aung <thetooaung07@gmail.com>
2022-09-19 14:43:48 +0630

    Assignment 2 Updated

--- assignment2.cpp
+++ assignment2.cpp
@@ -2,170 +2,228 @@
 // Thet Oo Aung
 
 #include<iostream>
-#include<fstream>
 #include<string>
+#include<fstream>
 
 using namespace std;
 
+const int MAXROW = 10;
+string USERNAME[MAXROW] = {};
+string USERID[MAXROW] = {};
+string USERPASSWORD[MAXROW] = {};
+int AGE[MAXROW] = {};
+int AMOUNT[MAXROW] = {};
+string LOCATION[MAXROW] = {};
+
 void performInsert();
+void perfromDisplay();
 void performSearchByUsername();
 void performUpdate();
+void performSaveAndExit();
 
-int id, amount, age;
-char username[50], password[50], location[100];
-int willContinue;
-int selectedOperation;
 
 int main()
 {
+    int selectedOperation;
+
+
+
     do
     {
-
         cout << "Welcome to Registration Program" << endl;
         cout << "Press";
-        cout << "\t 1) to insert data" << endl;
-        cout << "\t 2) to find data" << endl;
-        cout << "\t 3) to update data" << endl;
+        cout << "\t 1) to Insert data" << endl;
+        cout << "\t 2) to Show data" << endl;
+        cout << "\t 3) to Find data" << endl;
+        cout << "\t 4) to Update data" << endl;
+        cout << "\t 5) to Save and Exit from Program" << endl;
 
-        cout << "Choose One of the numbers to perform Operation:  ";
+        cout << "Choose Operation:  ";
         cin >> selectedOperation;
-
-
 
         switch(selectedOperation)
         {
         case 1:
             performInsert();
+            system("CLS");
             break;
-
         case 2:
+            system("CLS");
+            perfromDisplay();
+
+            break;
+        case 3:
+            system("CLS");
             performSearchByUsername();
             break;
 
-        case 3:
+        case 4:
             performUpdate();
             break;
 
-        case 4:
-            willContinue = 4;
+        case 5:
+            performSaveAndExit();
+            cout<< "Save Successful and Exiting..." << endl;
+            selectedOperation = 6;
+
             break;
         }
 
     }
-    while(willContinue != 4);
-
+    while(selectedOperation != 6);
 
 }
+
 
 
 void performInsert()
 {
+    char name[50];
+    char id[20];
+    char pass[20];
+    char location[100];
+    int age;
+    int amount;
 
-    cout << "\nRegistering New User\n" << endl;
+    cin.ignore();
+    cout << "\n";
+    cout << "Enter Id: " ;
+    cin.getline(id,20);
 
-    cout << "Enter Your ID: ";
-    cin>> id;
-    cout << "Enter Your Name: ";
-    cin >> username;
-    cout << "Enter Your Password: ";
-    cin >> password;
-    cout << "Enter Your Age: ";
-    cin >> age;
-    cout << "Enter Your Amount: ";
-    cin >> amount;
-    cout << "Enter Your Location: ";
-    cin >> location;
+    cout << "Enter Name: " ;
+    cin.getline(name,50);
+    cout << "Enter Password: " ;
+    cin.getline(pass,20);
+    cout << "Enter Age: " ;
+    cin >> age ;
+    cout << "Enter Amount: " ;
+    cin >> amount ;
+    cin.ignore();
+    cout << "Enter Location: " ;
+    cin.getline(location,100);
 
-    ofstream fout;
-    fout.open("profile.txt", ios::out);
+    for(int x = 0; x < MAXROW; x++)
+    {
+        if(USERID[x] == "\0")
+        {
+            USERID[x] = id;
+            USERNAME[x] = name;
+            USERPASSWORD[x] = pass;
+            AGE[x] = age;
+            AMOUNT[x] = amount;
+            LOCATION[x] = location;
+            cout << "Insert Successfully" << endl;
 
 
-//    fout << "ID       : " << id << endl;
-//    fout << "Name     : " << username << endl;
-//    fout << "Password : " << password << endl;
-//    fout << "age      : " << age << endl;
-//    fout << "amount   : " << amount << endl;
-//    fout << "location : " << location << endl;
-//    fout << endl;
+            cout << endl;
+            cout << endl;
+            break;
+        }
+        else
+        {
+            cout << "Operation Error"<< endl;
+        }
+    }
+}
+
+void perfromDisplay()
+{
+
+    cout << "Current Data" << endl;
+    cout << "=============================================================" << endl;
+    cout << "id" << "\t" <<  "name" << "\t" << "pass" << "\t" << "age" << "\t" << "amount" << "\t" << "location" << "\t" << endl;
+
+    for (int x = 0;  x < MAXROW; x++)
+    {
+        if(USERID[x] != "\0")
+        {
+
+            cout << USERID[x] << "\t" << USERNAME[x]  << "\t"  << USERPASSWORD[x] << "\t" << AGE[x]  << "\t" << AMOUNT[x] << "\t" << LOCATION[x]  << "\t"  << endl;
+        }
+
+    }
 
 
-    fout << id << "\t" << username << "\t" << password << "\t"<< age << "\t"<< amount << "\t" << location << "\t" << endl;
-    fout.close();
+    cout << "=============================================================" << endl;
 
-    cout << endl;
-    cout << "Insert Data Successfully~"<< endl;
-    cout << endl;
 
 }
+
 void performSearchByUsername()
 {
 
-    cout << "\nSearch User By Name\n" << endl;
 
-    ifstream fin;
-    fin.open("Profile.txt");
-    string temp;
-    string line;
-    string inputUsername;
+    string queryUsername;
+    cout << "Search by Username: " ;
+    cin.ignore();
+    getline(cin, queryUsername);
 
-    cout << "Search by Username: ";
-    cin >> inputUsername;
+    cout << "\nSearch Result" << endl;
+    cout << "=============================================================" << endl;
+    cout << "id" << "\t" <<  "name" << "\t" << "pass" << "\t" << "age" << "\t" << "amount" << "\t" << "location" << "\t" << endl;
 
 
-    if(fin.is_open())
+    for (int x = 0;  x < MAXROW; x++)
     {
-        while(getline(fin, temp))
+        if(USERNAME[x] == queryUsername)
         {
-            line.append(temp);
-            if(line.find(inputUsername) != string::npos)
-            {
-                cout << temp << endl;
-                break;
-            }
-            else
-            {
-                cout<< "\nUser not found!" << endl;
-            }
+
+            cout << USERID[x] << "\t" << USERNAME[x]  << "\t"  << USERPASSWORD[x] << "\t" << AGE[x]  << "\t" << AMOUNT[x] << "\t" << LOCATION[x]  << "\t"  << endl;
         }
-        fin.close();
+
     }
+
 
 }
 void performUpdate()
 {
 
+    system("CLS");
+    char name[50];
+    cin.ignore();
+    string queryUsername;
+    cout << "Enter Username: ";
+    getline(cin, queryUsername);
 
-  cout << "\Update User\n" << endl;
-
-    ifstream fin;
-    fin.open("Profile.txt");
-    string temp;
-    string line;
-    string inputUsername;
-
-    cout << "Type username to update: ";
-    cin >> inputUsername;
-
-
-    if(fin.is_open())
+    for (int x = 0;  x < MAXROW; x++)
     {
-        while(getline(fin, temp))
+        if(USERNAME[x] == queryUsername)
+
+
         {
-            line.append(temp);
-            if(line.find(inputUsername) != string::npos)
-            {
-                cout << temp << endl;
-                break;
-            }
-            else
-            {
-                cout<< "\nUser not found!" << endl;
-            }
+
+            cout << "Enter Name to Update:  ";
+
+            cin.getline(name,50);
+
+            USERNAME[x] = name;
+
+
+
+            cout << "Update Successful" << endl;
+
+            perfromDisplay();
+            break;
         }
-        fin.close();
+
     }
+}
 
 
+void performSaveAndExit()
+{
 
+
+    ofstream fout;
+    fout.open("Profile.txt",  ios::out);
+
+    for(int x = 0; x< MAXROW; x++)
+    {
+        if(USERID[x] != "\0")
+        {
+            fout << USERID[x] << "\t" << USERNAME[x]  << "\t"  << USERPASSWORD[x] << "\t" << AGE[x]  << "\t" << AMOUNT[x] << "\t" << LOCATION[x]  << "\t"  << endl;
+        }
+        else break;
+    }
 
 }
